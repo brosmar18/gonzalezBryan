cd394e5d5a78ed2280d66aa824f094945c4b123f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-29 16:40:27 -0500

    hooks setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {styles} from '../styles';
+import {navLinks} from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
   return (
     <div>
       Navbar
