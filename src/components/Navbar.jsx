55c489691aa17b13d184cc6264c2acd08107dc64
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-29 16:42:11 -0500

    scroll logic added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,19 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+  }, [])
+
   return (
     <div>
       Navbar
