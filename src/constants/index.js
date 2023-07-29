835f993f48e0266a41c2a770a56dc4efea0470f6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-29 16:20:25 -0500

    navLinks added

--- src/constants/index.js
+++ src/constants/index.js
@@ -30,3 +30,18 @@ import {
     sedgwick,
     unitypoint
 } from '../assets';
+
+export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
