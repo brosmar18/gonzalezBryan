03ba2e6ca2affd47f3b3024128b4a3489e3b4b2b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 14:53:36 -0500

    styles added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
       </div>
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{ y: [0, 24, 0]}}
               transition={{
