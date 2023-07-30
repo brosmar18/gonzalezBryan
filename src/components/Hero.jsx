c160f02634180870db495c1bddda9db088fadab5
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 10:34:12 -0500

    title and desc added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {styles} from '../styles';
+import {motion} from 'framer-motion';
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section>
+      <div>
+        <div>
+          <div />
+          <div />
+        </div>
+        <div>
+          <h1>
+            Hi, I'm 
+            <span>Bryan</span>
+          </h1>
+          <p>
+            Full-Stack Software Developer
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
