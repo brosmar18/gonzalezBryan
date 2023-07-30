d1ca3f3c4ca6aecc73b0e32b9157fdca4aee17a3
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 17:53:49 -0500

    span and component render added

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -12,7 +12,12 @@ const Wrapper = (Component, idName) => function HOC() {
             viewport={{once: true, amount: 0.25}}
             className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
         >
-            
+            <span className="hash-span" id={idName}>
+                &nbsp;
+            </span>
+            <Component />
         </motion.section>
-    )
-}
+    );
+};
+
+export default Wrapper;
