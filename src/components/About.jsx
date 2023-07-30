ca019f6e6eb698cf637b74d0d3fc09f90f431a85
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 18:03:13 -0500

    desc text added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,6 +14,12 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in Javascrict, HTML, CSS, and exprtise in frameworks like React, Node.js, Next.js, and Three.js. I collaborate closely with clients to create efficient, scalable, and user-friendly solutions to real-world tech problems. Let's work together to bring your ideas to life!
+      </motion.p>
     </>
   )
 }
