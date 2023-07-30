1931ac68e7f8bc77c2c8fe4142ade00d7d6989cd
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 14:47:21 -0500

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MediaQuery from '../hooks/MediaQuery';
 import {motion} from 'framer-motion';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
 import {styles} from '../styles';
 import SocialsIcons from './SocialsIcons';
 
@@ -9,17 +8,17 @@ const Header = () => {
 
   const isAboveLarge = MediaQuery("(min-width: 1060px)");
   return (
-    <section id='home' className='md:flex md:justify-between md:items-center gap-16 md:h-full py-10'>
-      <div className='basic-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2'>
+    <section id='home' className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10">
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
         {isAboveLarge ? (
           <div>
-            <img alt='profile' src="/profile.png" />
+            <img alt='profile' src="/profile.png" className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" />
           </div>
         ) : (
-          <img alt='profile' src="/profile.png" />
+          <img alt='profile' src="/profile.png" className="z-10 w-full max-w-[400px] md:max-w-[600px]" />
         )}
       </div>
-      <div>
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -30,55 +29,22 @@ const Header = () => {
             visible: {opacity: 1, x: 0},
           }}
         >
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white z-10 text-center`}>
             Hi, I'm <span className='text-[#915EFF]'>Bryan</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 text-center`}>
             full-stack Software Developer
           </p>
         </motion.div>
-
         <motion.div
-          className="flex mt-5 justify-center md:justify-start"
+          className="flex mt-5 justify-center"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
-          }}
-        >
-          <AnchorLink
-            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
-              hover:bg-blue hover:text-white transition duration-500"
-            onClick={() => setSelectedPage("contact")}
-            href="#contact"
-          >
-            Contact Me
-          </AnchorLink>
-          <AnchorLink
-            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
-            onClick={() => setSelectedPage("contact")}
-            href="#contact"
-          >
-            <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair">
-              Let's talk.
-            </div>
-          </AnchorLink>
-        </motion.div>
-
-    
-        
-        <motion.div
-          className='flex mt-5 justify-center md:justify-start'
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5}}
-          transition={{ delay: 0.4, duration: 0.5}}
-          variants={{
-            hidden: {opacit: 0, x: -50},
-            visible: {opacity: 1, x: 0},
           }}
         >
           <SocialsIcons />
