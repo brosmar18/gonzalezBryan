3fa949d242700aef96d0326c022d8ca936ef3bd9
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 18:07:50 -0500

    services rendered

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,8 @@ import { services } from "../constants";
 import { Wrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const Card = ({index, title, icon}) => (
-  <Tilt className='xs:2-[250px] w-full'>
+const Card = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -33,23 +33,34 @@ const Card = ({index, title, icon}) => (
       </div>
     </motion.div>
   </Tilt>
-)
+);
 
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant}>
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in Javascrict, HTML, CSS, and exprtise in frameworks like React, Node.js, Next.js, and Three.js. I collaborate closely with clients to create efficient, scalable, and user-friendly solutions to real-world tech problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <Card key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Wrapper(About, "about");
