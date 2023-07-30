a69831489c481bf00d3b8f8c4cecb14734bd5b54
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 11:02:15 -0500

    imports added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {styles} from '../styles';
 import {motion} from 'framer-motion';
 
+
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
