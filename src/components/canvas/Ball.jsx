8aa66b628b519d5a0ab48f6a67f5f03d46d85bf1
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 18:54:14 -0500

    imports added

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 
 const Ball = () => {
   return (
