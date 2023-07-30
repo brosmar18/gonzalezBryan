c85b9bfba918e3ed8d80a62183b8e512b8f7775e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 13:39:40 -0500

    dependency added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import {styles} from '../styles';
+import MediaQuery from '../hooks/MediaQuery';
 import {motion} from 'framer-motion';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Header = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-      <div className={`abslute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915Eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm 
-            <span className='text-[#915EFF]'>Bryan</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Full-Stack Software Developer
-          </p>
-        </div>
-      </div>
-    </section>
+    <div>
+      
+    </div>
   )
 }
 
-export default Header;
+export default Header
