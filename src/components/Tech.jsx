3a2ef1c11bb9f205b0accc6aa08f74052e6cf7f7
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 18:55:52 -0500

    Tech section setup

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,8 +5,12 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>
-      Tech
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas />
+        </div>
+      ))}
     </div>
   )
 }
