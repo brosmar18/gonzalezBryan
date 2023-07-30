713f95312a97c8c6db6512c22632c75a6e2335ad
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 18:40:42 -0500

    company_name rendered

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -34,6 +34,9 @@ const ExperienceCard = ({experience}) => {
     >
       <div>
         <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+        <p className="text-secondary text-[16px] font-semibold">
+          {experience.company_name}
+        </p>
       </div>
     </VerticalTimeline>
   )
