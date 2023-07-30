7d1f01ad098c889036d59f950e80fe4146d0ad86
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 13:35:53 -0500

    media logic added

--- src/hooks/MediaQuery.jsx
+++ src/hooks/MediaQuery.jsx
@@ -4,8 +4,16 @@ const MediaQuery = (query) => {
     const [matches, setMatches] = useState(false);
 
     useEffect(() => {
+        const media = window.matchMedia(query);
+        if (media.matches !== matches) {
+            setMatches(media.matches);
+        }
+        const listener = () => setMatches(media.matches);
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, [matches, query]);
 
-    }, [])
+    return matches;
 }
 
 export default MediaQuery;
