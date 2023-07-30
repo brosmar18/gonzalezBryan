1b9e53a7b8eef54834e783198c3385d43e5dc75f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 14:52:52 -0500

    animated circle added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,6 +51,23 @@ const Header = () => {
         </motion.div>
 
       </div>
+      <div>
+        <a href='#about'>
+          <div>
+            <motion.div
+              animate={{ y: [0, 24, 0]}}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop"
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            >
+
+            </motion.div>
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
