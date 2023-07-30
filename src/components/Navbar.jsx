ac620a2324b2797897698d29063939c8df68c88c
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 10:29:17 -0500

    mobile nav links styles added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,8 +73,10 @@ const Navbar = () => {
             onClick={() => setToggle(!toggle)}
             className='w-[28px] h-[28px] object-contain'
           />
-          <div>
-            <ul>
+          <div className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
@@ -82,6 +84,7 @@ const Navbar = () => {
                     setToggle(!toggle);
                     setActive(nav.title)
                   }}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${ active === nav.title ? "text-white" : "text-secondary"}`}
                 >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
