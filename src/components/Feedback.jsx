19422e2f18efffe594ebfcd1bc56f25d636fcdad
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 20:32:55 -0500

    FeedBack card component built, quote added

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -6,6 +6,22 @@ import { Wrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  designmation,
+  company,
+  image
+}) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px]"
+  >
+    <p className='text-white font-black text-[48px]'>"</p>
+  </motion.div>
+);
+
 const Feedback = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
@@ -16,6 +32,11 @@ const Feedback = () => {
           <p className={styles.sectionSubText}>What clients are saying</p>
           <h2 className={styles.sectionHeadText}>Testimonials</h2>
         </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
       </div>
     </div>
   )
