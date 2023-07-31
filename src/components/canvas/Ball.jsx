006820667ca0e4999c31f892ae19dee64411f41b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 19:02:15 -0500

    Ball func component built

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,13 +8,33 @@ import {
   useTexture,
 } from "@react-three/drei";
 import CanvasLoader from '../Loader';
+import { BallCanvas } from ".";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <div>
-      Ball
-    </div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
+};
 
-export default Ball
+export default BallCanvas;
