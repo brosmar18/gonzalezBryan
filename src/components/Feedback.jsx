52f4e9e48d941099e59aa7c109d60fbff004e7f6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 20:37:19 -0500

    position and company rendered

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -10,7 +10,7 @@ const FeedbackCard = ({
   index,
   testimonial,
   name,
-  designmation,
+  designation,
   company,
   image
 }) => (
@@ -24,9 +24,12 @@ const FeedbackCard = ({
       <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
 
       <div className="mt-7 flex justify-between items-center gap-1">
-        <div className="flex-1 flex flex-col">
-        <p className='text-white font-medium text-[16px]'>
+      <div className='flex-1 flex flex-col'>
+          <p className='text-white font-medium text-[16px]'>
             <span className='blue-text-gradient'>@</span> {name}
+          </p>
+          <p className='mt-1 text-secondary text-[12px]'>
+            {designation} of {company}
           </p>
         </div>
       </div>
