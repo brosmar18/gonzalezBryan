68e65be1f5f1c5c9c19e78eb0b6906223414d344
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 20:51:13 -0500

    title and subTittle added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,20 @@ import {motion} from 'framer-motion';
 import emailjs from '@emailjs/browser';
 import {styles} from '../styles';
 import {Wrapper} from '../hoc';
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div
+      className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+      <p className={styles.sectionSubText}>Get in touch</p>
+      <h3 className={styles.sectionHeadText}>Contact</h3>
+      </motion.div>
     </div>
   )
 }
