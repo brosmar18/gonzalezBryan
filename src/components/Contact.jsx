6c4328572ba63d42a7463ec78362a45dd4eeaa58
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 21:08:06 -0500

    contact form created

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,11 +65,11 @@ const Contact = () => {
 
   return (
     <div
-      className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'
+      className='xl:mt-12 flex xl:flex-row flex-col-reverse'
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='flex-[1] bg-black-100 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
@@ -121,8 +121,6 @@ const Contact = () => {
           </button>
         </form>
       </motion.div>
-
-
     </div>
   )
 }
