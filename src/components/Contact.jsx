8105e1efb14a71f730bae95ed483d93dc49d77e4
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 21:01:30 -0500

    hooks imported

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 import { motion } from 'framer-motion';
 import emailjs from '@emailjs/browser';
 import { styles } from '../styles';
