c5461eb154d92182faf91ff5fc35f01b9db793f1
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 22:17:05 -0500

    img added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -26,36 +26,29 @@ const ProjectCard = ({
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
-        <div className="relative w-full h-[230px]">
+        <div className='relative w-full h-[230px]'>
           <img
             src={image}
-            alt={name}
-            className="w-full h-full object-cover rounded-2xl"
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
           />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-1">
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer hover:text-purple-500 transition-colors"
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
             >
               <AiFillGithub />
-            </div>
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer hover:text-purple-500 transition-colors"
-            >
-              <AiFillEye />
             </div>
           </div>
         </div>
 
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">
-            {description}
-          </p>
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
         </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
@@ -69,7 +62,6 @@ const ProjectCard = ({
     </motion.div>
   );
 };
-
 const Works = () => {
   return (
     <>
@@ -84,14 +76,15 @@ const Works = () => {
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           The projects featured here provide a comprehensive representation of my capabilities and experience, serving as tangible demonstrations of my work. Each project comes with a succinct description, alongside links to the corresponding code repositories and live demos. These projects collectively highlight my proficiency in tackling intricate problems, utilizing various technologies, and ensuring effective project management.
-        </motion.p>
+          </motion.p>
       </div>
+
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 export default Wrapper(Works, "");
