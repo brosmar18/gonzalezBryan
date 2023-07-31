1b13c600b9b6019d43b7eaf40fc9f31e3f8baaf0
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 19:54:02 -0500

    desc added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,6 +15,15 @@ const Works = () => {
       <p className={`${styles.sectionSubText} `}>My work</p>
       <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
       </motion.div>
+
+      <div className="w-full flex">
+      <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          The projects featured here provide a comprehensive representation of my capabilities and experience, serving as tangible demonstrations of my work. Each project comes with a succinct description, alongside links to the corresponding code repositories and live demos. These projects collectively highlight my proficiency in tackling intricate problems, utilizing various technologies, and ensuring effective project management.
+        </motion.p>
+      </div>
     </>
   )
 }
