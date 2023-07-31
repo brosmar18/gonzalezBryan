7e7741975df640fd629702a84ab1ff3c9607688d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-30 20:00:44 -0500

    projectCard func component setup

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,21 +8,34 @@ import { Wrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = () => {
+  return (
+    <div>
+      Project Card
+    </div>
+  );
+};
+
 const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-      <p className={`${styles.sectionSubText} `}>My work</p>
-      <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
       </motion.div>
 
-      <div className="w-full flex">
-      <motion.p
+      <div className='w-full flex'>
+        <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           The projects featured here provide a comprehensive representation of my capabilities and experience, serving as tangible demonstrations of my work. Each project comes with a succinct description, alongside links to the corresponding code repositories and live demos. These projects collectively highlight my proficiency in tackling intricate problems, utilizing various technologies, and ensuring effective project management.
         </motion.p>
+      </div>
+        <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
       </div>
     </>
   )
