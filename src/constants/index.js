358cba0ca954def4b407f9d23cbdbd6860e13d68
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-08 23:40:21 -0500

    technologies removed from constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -64,65 +64,6 @@ export const navLinks = [
     }
   ];
   
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
-    {
-      name: "Github",
-      icon: github,
-    },
-
-  ];
 
   const experiences = [
     {
@@ -292,4 +233,4 @@ export const navLinks = [
     },
   ];
 
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, experiences, testimonials, projects };
