8da08076a74160b49c53dd733f27b6ba1a7296f8
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-09 11:59:52 -0500

    Title and subtitle added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { styles } from '../styles';
 import { Wrapper } from '../hoc';
 import { fadeIn } from '../utils/motion';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 
-{ /* 
-
-  async function getProjects() {
-    try {
-      const response = await fetch('http://localhost:1337/api/projects?populate=image', { method: "GET"});
-      const data = await response.json();
-
-      setProjects(data.data);
-      console.log(data.data);
-    } catch (e) {
-      console.log("Error fetching projects", e);
-    }
-  }
-
-  useEffect(() => {
-    getProjects();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-*/}
-const Projects = () => {
-  return (
-    <div>
-      <h1>Projects</h1>
-    </div>
-  )
+const ProjectCard = () => {
+    return (
+        <motion.div>
+            <h1>Project Card</h1>
+        </motion.div>
+    )
 }
 
-export default Projects;
-  
+
+const Projects = () => {
+    const [projects, setProjects] = useState([]);
+
+    async function getProjects() {
+        try {
+            const response = await fetch('http://localhost:1337/api/projects?populate=image', { method: "GET" });
+            const data = await response.json();
+
+            setProjects(data.data);
+            console.log(data.data);
+        } catch (e) {
+            console.log("Error fetching projects", e);
+        }
+    }
+
+    useEffect(() => {
+        getProjects();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+    return (
+        <>
+            <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__skills-item app__flex"
+            >
+                <p className={`${styles.sectionSubText} `}>My work</p>
+                <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
+            </motion.div>
+
+        </>
+    )
+}
+export default Wrapper(Projects, "");
