48e60dcab3691f848ec3f7b4cf121680a7918c4f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-09 09:52:43 -0500

    Successfully fetching experiences

--- src/components/JobExperience.jsx
+++ src/components/JobExperience.jsx
@@ -3,9 +3,26 @@ import { VerticalTimelineElement, VerticalTimeline } from 'react-vertical-timeli
 import { motion } from 'framer-motion';
 import 'react-vertical-timeline-component/style.min.css';
 import { styles } from '../styles';
-import { Wrapper } from '../utils/motion';
+import { Wrapper } from '../hoc';
 
 const JobExperience = () => {
+  const [experiences, setExperiences] = useState([]);
+
+  async function getExperiences() {
+    try {
+      const response = await fetch('http://localhost:1337/api/experiences?populate=image', { method: "GET"});
+      const data = await response.json();
+      setExperiences(data.data);
+      console.log(data.data)
+    } catch (error) {
+      console.log("Error fetching experiences", e);
+    }
+  }
+
+  useEffect(() => {
+    getExperiences();
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>
       <h1>Job Experience</h1>
