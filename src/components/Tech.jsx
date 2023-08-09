b05d309b565c44bf3810e15000704fc622baee07
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-08 22:01:09 -0500

    async func added to fetch technologies

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { BallCanvas } from "./canvas";
 import { Wrapper } from "../hoc";
-import { technologies } from "../constants";
+
+
 
 const Tech = () => {
+  const [technologies, setTechnologies] = useState([]);
+
+  async function getTechnologies() {
+    try {
+      const response = await fetch("http://localhost:1337/technologies", { method: "GET" });
+      const data = await response.json();
+      setTechnologies(data);
+    } catch (e) {
+      console.log("Error fetching technologies:", e);
+    }
+  }
+
+  useEffect(() => {
+    getTechnologies();
+  }, []); //// eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
