416be089bcb786eab77dac396945c2578aee9bea
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-09 09:38:29 -0500

    imports added

---
+++ src/components/JobExperience.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+import { VerticalTimelineElement, VerticalTimeline } from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+import 'react-vertical-timeline-component/style.min.css';
+import { styles } from '../styles';
+import { Wrapper } from '../utils/motion';
+
+const JobExperience = () => {
+  return (
+    <div>
+      <h1>Job Experience</h1>
+    </div>
+  )
+}
+
+export default JobExperience
